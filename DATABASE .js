a58480d947ea69a43c6df9d64080865fc6bcc815
SyahirahMF <129152809+SyahirahMF@users.noreply.github.com>
2023-03-30 23:53:53 +0800

    Add files via upload

---
+++ DATABASE .js
@@ -0,0 +1,55 @@
+let message = "benr"
+console.log(message, typeof message)
+
+message = 123
+console.log(message, typeof message)
+
+//let str = "BENR"
+//let num = 3523
+
+//let course = str + num
+//console.log(course, typeof course)
+
+let str = "1"
+let num = 1
+
+let total = parseInt(str) + num
+console.log(total, typeof total)
+
+let str2 = "1.258"
+let num2 = 1
+
+let total2 = parseFloat(str2) + num2
+console.log(total2, typeof total2)
+
+// array ["str1","str2","str3"]  object: data = {name:"NSF", age:50, faculty:"FKEKK"}
+
+//data.name -> NSF
+//data.age -> 50
+
+let data = ["benr", 3523, true]
+
+console.log(data[0])
+console.log(data[1])
+console.log(data[2])
+
+let user = { name: "SY", age: 23, faculty: "FKEKK"}
+console.log(user.name)
+console.log(user.age)
+console.log(user.faculty)
+
+let messages = [ {name: "usr1", date: "1/4/2023", msg: "good video", like: 1000}, 
+{name: "usr2", date: "2/4/2023", msg: "very good video", like: 3000}]
+
+console.log(messages[1].msg) // very good video
+console.log(messages[1].like)
+
+
+
+
+
+
+
+
+
+
